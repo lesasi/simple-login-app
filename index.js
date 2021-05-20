b63b6d7a06e258cdf24795abd847425b61def092
lesasi <nevinusa@gmail.com>
2021-05-20 22:35:35 +0530

    Small changes for deployment

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const app = require('./src/app');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const path = require("path")
+
+const userRouter = require('./src/routers/user');
+require('./src/db/mongoose');
+
+const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cookieParser());
+
+// for deployment
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// routers here
+app.use(userRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log('Server listening at port ' + port);
