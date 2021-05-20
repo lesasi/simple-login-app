5ed75603f1f1376022d6caf2942e7079a1f33cab
lesasi <nevinusa@gmail.com>
2021-05-20 19:54:14 +0530

    Made changes to file structure for deployment

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001';
-
 export const kfetch = async (url, body, type = 'GET') => {
     const options = {
 		method: type,
@@ -13,6 +11,5 @@ export const kfetch = async (url, body, type = 'GET') => {
 
 
 export const axiosObj = axios.create({
-    baseURL,
     withCredentials: true
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
+const path = require("path")
 
 const userRouter = require('./routers/user');
 require('./db/mongoose');
@@ -11,7 +12,14 @@ app.use(express.json());
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser());
 
+// for deployment
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 // routers here
 app.use(userRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
