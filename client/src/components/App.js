f9227b62e95bfdf8f451bcd986901d1c80ffedea
lesasi <nevinusa.com>
2021-06-07 23:52:53 +0530

    Refactored the structure of App code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Router, Route, Link, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-
+import reducers from '../reducers';
 import getUserDetails from '../actions/getUserDetails';
 import AppRouter, { history } from '../routers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 const App = () => {
 
     const [loading, setLoading] = useState(false);
-    const dispatch = useDispatch();
 
     // to get user data - if none, redirect to login page
     useEffect(() => {
         const fetchUser = async () => {
             setLoading(true);
             const { data, error } = await getUserDetails();
-            if(error){
-                history.push('/login')
-            }else {
-                dispatch({
+            if(data){
+                store.dispatch({
                     type: 'INIT_USER',
                     payload: {
-                        ...data
+                        user: data.user
                     }
                 });
                 setLoading(false);
@@ -33,9 +35,9 @@ const App = () => {
 
 
     return(
-        <div>
+        <Provider store={store}>
             <AppRouter />
-        </div>
+        </Provider>
     );
 };
 
